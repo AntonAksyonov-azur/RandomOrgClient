dae642e3dfd4a9d78112a7931408034fcc28a7bf
arazect <za3a24@yandex.ru>
2013-11-28 17:34:12 +0700

    Class rename

--- RandomOrgClient/com/andaforce/arazect/data/RandomPresetRecord.cs
+++ RandomOrgClient/com/andaforce/arazect/data/RandomPresetRecord.cs
@@ -3,12 +3,16 @@ using RandomOrgClient.com.andaforce.arazect.format;
 
 namespace RandomOrgClient.com.andaforce.arazect.data
 {
-    public class RandomSettingsRecord
+    public class RandomPresetRecord
     {
         public String Name;
         public int Min;
         public int Max;
-        public RandomNumberType RandomNumberType;
         public int Count;
+
+        public override string ToString()
+        {
+            return String.Format("{0} ({1} -> {2})", Name, Min, Max);
+        }
     }
 }
