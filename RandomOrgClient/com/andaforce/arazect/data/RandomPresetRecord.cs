5bcd2dab4b85fe9ed471b0ffba7b9b20164cbb19
arazect <za3a24@yandex.ru>
2013-11-29 10:11:50 +0700

    Added Equals() method to RandomPresetRecord

--- RandomOrgClient/com/andaforce/arazect/data/RandomPresetRecord.cs
+++ RandomOrgClient/com/andaforce/arazect/data/RandomPresetRecord.cs
@@ -4,6 +4,31 @@ namespace RandomOrgClient.com.andaforce.arazect.data
 {
     public class RandomPresetRecord
     {
+        protected bool Equals(RandomPresetRecord other)
+        {
+            return string.Equals(Name, other.Name) && Min == other.Min && Max == other.Max && Count == other.Count;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((RandomPresetRecord) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Min;
+                hashCode = (hashCode * 397) ^ Max;
+                hashCode = (hashCode * 397) ^ Count;
+                return hashCode;
+            }
+        }
+
         public String Name;
         public int Min;
         public int Max;
